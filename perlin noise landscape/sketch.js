c72856bf8d225128c5d45770785bfd1da20569d2
Cypress707 <rydertaylor707@gmail.com>
2024-10-02 15:16:49 -0600

    made the flagpole and the key inputs

--- perlin noise landscape/sketch.js
+++ perlin noise landscape/sketch.js
@@ -5,12 +5,13 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let rTime = 5
+let rTime1 = 5
 let rInterval = 0.02
 let rWidth = 1
+let maxY = 0
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(20)
+  
 }
 
 function draw() {
@@ -18,20 +19,25 @@ function draw() {
 
 
   mountainRange();
-  makeFlag();
+ 
+  makeFlag(flagx, maxY);
 }
-function makeFlag(){
-  fill(0,0,0);
-  triangle(xFlag, fHeight += 15, x += 15, fHeight += 10, xFlag, fHeight += 5)
-  if(rectHeight > rectHeight){
-    xFlag = x;
-    flagHeight = rectHeight;
-  }
-}
+//function makeFlag(){
+ // fill(0,0,0);
+ // triangle(xFlag, fHeight += 15, x += 15, fHeight += 10, xFlag, fHeight += 5);
+ // if(rectHeight > flagHeight){
+ //   let xFlag = x;
+ //   let flagHeight = rectHeight;
+  //}
+//}
 
 function mountainRange(){
+
+  
+  
+  let rTime = rTime1
   randomSeed(2);
-  for(let x = 0; x < width; x += 1){
+  for(let x = 0; x < width; x += rWidth){
   fill(0,0,0);
   let rectHeight = noise(rTime);
   rectHeight = map(rectHeight,0, 1, 50, 500);
@@ -39,13 +45,28 @@ function mountainRange(){
   rect(x, height, rWidth, -rectHeight)
 
   rTime += rInterval;
+  if(height - rectHeight > maxY){
+    flagx = x;
+    maxY = height - rectHeight;
+  }
   }
 }
 
-function buttonIsPressed(){
-  if(key = a){
-    if(rWidth >= 1){
-      rWidth = rWidth += 1
+function keyPressed(){
+  if(key === "a"){
+    if(rWidth > 1){
+      rWidth = rWidth -= 1;
     }
   }
+  if(key === "d"){
+    if(rWidth < 100){
+      rWidth = rWidth += 1;
+    }
+  }
+}
+
+function makeFlag(x,y){
+  fill(0,0,0);
+  triangle(x,y + 12.5, x + 10, y + 6.25, x, y);
+  line(x, y + 35, x, y);
 }
